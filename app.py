8715e3351f76b176f6fa901b897340adafcac9d5
Mathias Brasche <mathias@mbrasche.de>
2017-11-21 21:49:25 +0100

    add list_user

--- app.py
+++ app.py
@@ -22,6 +22,23 @@ def list_users():
     conn.close()
     return jsonify({'user_list': api_list}), 200
 
+def list_user(user_id):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully")
+    api_list = []
+    cursor = conn.execute(
+        "SELECT * from users where id=?", (user_id,))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        user = {}
+        user['username'] = data[0][0]
+        user['name'] = data[0][1]
+        user['email'] = data[0][2]
+        user['password'] = data[0][3]
+        user['id'] = data[0][4]
+    conn.close()
+    return jsonify(user), 200
+    
 
 @app.route("/api/v1/info")
 def home_index():
@@ -42,9 +59,16 @@ def home_index():
     conn.close()
     return jsonify({'api_version': api_list}), 200
 
+
 @app.route('/api/v1/users', methods=['GET'])
 def get_users():
     return list_users()
+
+
+@app.route('/api/v1/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    return list_user(user_id)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
